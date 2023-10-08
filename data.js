f361484a0a061a25fc9cdf0e5298e5602d4c75f4
AbdulHalim24 <147292538+AbdulHalim24@users.noreply.github.com>
2023-10-08 10:59:11 +0700

    Add files via upload
    Maaf kalau melenceng jauh dari tugasnya, baru bisa segini, masih sangat sangat pemula dalam hal javacript, sekali lagi maaf hehe

---
+++ data.js
@@ -0,0 +1,76 @@
+// variabel kucing tuti data 1
+const nama = " Data 1 Usia Kucing Tuti";
+const jumlahDewasa = "4 kucing dewasa";
+const jumlahKecil = "1 kucing kecil";
+let usia = 3 ;
+let usiaSatu = 5 ;
+let usiaDua = 2 ;
+let usiaTiga = 12 ;
+let usiaEmpat = 7 ;
+
+console.log(nama);
+console.log(usia);
+console.log(usiaSatu);
+console.log(usiaDua)
+console.log(usiaTiga)
+console.log(usiaEmpat)
+console.log(jumlahDewasa)
+console.log(jumlahKecil)
+
+// variabel kucing nining data 1
+const namaNining = " Data 1 Usia Kucing nining";
+const jumlahDewasaNining = "4 kucing dewasa";
+const jumlahKecilNining = "1 kucing kecil";
+let usiaKucingNining = 4 ;
+let usiaSatuNining = 1 ;
+let usiaDuaNining = 15 ;
+let usiaTigaNining = 8;
+let usiaEmpatNining = 3 ;
+
+console.log(namaNining);
+console.log(usiaKucingNining);
+console.log(usiaSatuNining);
+console.log(usiaDuaNining)
+console.log(usiaTigaNining)
+console.log(usiaEmpatNining)
+console.log(jumlahDewasaNining)
+console.log(jumlahKecilNining)
+
+// variabel kucing tuti data 2
+const namaTutiDua = " Data 2 Usia Kucing Tuti";
+const jumlahDewasaTutiDua = "5 kucing dewasa";
+const jumlahKecilTutiDua = "0 kucing kecil";
+let usiaTutiDua = 9 ;
+let usiaSatuTuti = 16 ;
+let usiaDuaTuti = 6 ;
+let usiaTigaTuti = 8 ;
+let usiaEmpatTuti = 3 ;
+
+console.log(namaTutiDua);
+console.log(usiaTutiDua);
+console.log(usiaSatuTuti);
+console.log(usiaDuaTuti)
+console.log(usiaTigaTuti)
+console.log(usiaEmpatTuti)
+console.log(jumlahDewasaTutiDua)
+console.log(jumlahKecilTutiDua)
+
+//  variabel kucing nining data 2
+const namaNiningDua = " Data 2 Usia Kucing nining";
+const jumlahDewasaNiningDua = "4 kucing dewasa";
+const jumlahKecilNiningDua = "1 kucing kecil";
+let usiaKucingNiningDua = 10 ;
+let usiaSatuNiningDua = 5 ;
+let usiaDuaNiningDua = 6 ;
+let usiaTigaNiningDua = 1;
+let usiaEmpatNiningDua = 4 ;
+
+console.log(namaNiningDua);
+console.log(usiaKucingNiningDua);
+console.log(usiaSatuNiningDua);
+console.log(usiaDuaNiningDua)
+console.log(usiaTigaNiningDua)
+console.log(usiaEmpatNiningDua)
+console.log(jumlahDewasaNiningDua)
+console.log(jumlahKecilNiningDua)
+
